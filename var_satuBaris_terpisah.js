cec4ef168b49d023d8465bb0b30681c376d6f1f1
Komang Aswameda <aswameda18@gmail.com>
2026-05-22 17:14:50 +0800

    belajar 2

---
+++ var_satuBaris_terpisah.js
@@ -0,0 +1,8 @@
+// semua dalam satu baris
+var brand = 'honda', type = 'MVP', numberofWheels = 4, price;
+
+// baris terpisah (lebih baik dari pada satu baris)
+var brand = 'honda',
+type = 'MVP',
+numberofWheels = 4,
+price;
