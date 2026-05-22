14d5c4643346f47fe6b15d3173d30f8f096bac96
Komang Aswameda <aswameda18@gmail.com>
2026-05-22 17:44:44 +0800

    belajar object1

---
+++ js/object/object1.js
@@ -0,0 +1,18 @@
+// object ditandai dengan {} siku kurung
+
+var car = {};
+
+//notasi objek, lebih disukai
+
+//setiap properti dipisahkan oleh koma & key-value dipisahkan //oleh titik-dua.
+
+//Tidak boleh menggunakan keyword var
+var myCar = {
+  brand: "Honda",
+  year: 2011,
+};
+
+//cara yg sama dengan sintaks associative-array
+var yourCar = {};
+yourCar["brand"] = "Toyota";
+yourCar["year"] = 2012;
