b71fc7a84f3e98956b475e046db17d030faab360
Komang Aswameda <aswameda18@gmail.com>
2026-05-23 14:09:36 +0800

    belajar function1

---
+++ js/function/function1.js
@@ -0,0 +1,10 @@
+// function sederhana
+
+function sum(a,b) {
+    //signature, nama = sum, parameter = a & b
+    return a + b;
+    //body
+};
+
+// eksekusi function, tampilkan hasil di console
+console.log(sum(1,2));
