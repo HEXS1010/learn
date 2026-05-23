e7241032f30c8cbdb673f4a7931305b1319a9811
Komang Aswameda <aswameda18@gmail.com>
2026-05-23 17:29:24 +0800

    belajar function5

--- js/function/function4.js
+++ js/function/function4.js
@@ -7,6 +7,32 @@ var fn = function() {
 
     function fn_test() {
         var b = 20;
-        console.log('#')
+        console.log('#fn_test');
+        console.log(a);
     }
+
+    function fn_test2() {
+        console.log('#fn_test2');
+        console.log(a);
+
+        function fn_test2_child() {
+            console.log('##fn_test2_child');
+            console.log('a');
+
+            console.log(a);
+            console.log('##call fn_test()');
+
+            fn_test();
+        }
+
+        fn_test2_child();
+    }
+
+    fn_test();
+    fn_test2();
+
+    console.log('fn');
+    // console.log(b);
 }
+
+fn();
