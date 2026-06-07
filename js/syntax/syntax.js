9d1b4f6f8bb4308904b6177d0d60e88498ba5986
Komang Aswameda <aswameda18@gmail.com>
2026-06-07 15:29:07 +0800

    belajar syntax js

---
+++ js/syntax/syntax.js
@@ -0,0 +1,19 @@
+// Kata kunci JavaScript digunakan untuk mendefinisikan tindakan yang akan dilakukan.
+let x = 5;
+const fname = "jhon";
+
+console.log(fname + " umur " + x + " tahun");
+
+// Variabel adalah wadah untuk menyimpan nilai data
+// Variabel harus diidentifikasi dengan nama yang unik 
+let y;
+y = 6;
+console.log(y);
+
+// operator javascript
+let a = 5;
+let b = 6;
+let sum = a + b;
+
+console.log(sum);
+console.log(sum * 10);
