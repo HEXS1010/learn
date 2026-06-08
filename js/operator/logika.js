0f8cc51bc04eeefbdeb05da9589fa3aa21ad3fc8
Komang Aswameda <aswameda18@gmail.com>
2026-06-08 22:08:29 +0800

    belajar logika

---
+++ js/operator/logika.js
@@ -0,0 +1,24 @@
+// dalam logika kita belajar and dan or ok 
+
+let x = 7;
+let y = 8;
+
+if (x < 10 && y > 5) {
+    console.log("mantap");
+} else {
+    console.log("gk mantap");
+}
+
+// or
+let a = 6;
+let b = 8;
+let z = (a > 5 || b < 2);
+
+console.log(z);
+
+// not
+let h = (5 == 8); // false
+let n = !(5 == 8); // true
+
+console.log(h);
+console.log(n);
