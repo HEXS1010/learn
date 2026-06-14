279f903bc79a9717f0ee3840a8cbd28554eccad7
Komang Aswameda <aswameda18@gmail.com>
2026-06-14 16:46:33 +0800

    selesai belajar string5

--- js/String/string5.js
+++ js/String/string5.js
@@ -4,3 +4,44 @@
 // sedangkan kita ingin mengelola datanya dalam bentuk number
 // maka sangat disarankan untuk melakukan konversi tipe data
 
+// contoh masalah tanpa konversi
+// ===========penjelasan============
+/* mengapa menjadi 11 ? karena saat ada tanda + maka js akan memiliki 2 fungsi
+penjumlahan matematika, atau penggabungan string. nah apa bila ada salah satu data string ia akan mengkonversi nilai lainnya menjadi string */
+console.log("=================================");
+const value1 = "1";
+const value2 = 1;
+
+const sum = value1 + value2;
+
+console.log(`${sum}`);
+
+// solusi seperti ini
+/* parselnt(string)
+mengkonversi dari sring ke number(bilangan bulat)*/
+
+/* parseFloat(string)
+mengkonversi dari string ke number (bilangan pecahan) */
+
+/* Number(string)
+mengkonversi dari string ke number(bilangan bulat atau pecahan) */
+
+/* number.toString()
+mengkonversi dari number ke string */
+
+console.log("======parseInt======");
+const a = parseInt("1");
+const b = 1;
+const c = a + b;
+console.log(c);
+console.log(`${parseInt("1.9")}`);
+console.log(`${parseInt("123anjay")}`);
+
+console.log("======parseFoat======");
+console.log(`${parseFloat("1.5")}`);
+console.log(`${parseFloat("1")}`);
+
+console.log("======Number======");
+console.log(`${Number("1")}`);
+console.log(`${Number("1.5")}`);
+console.log(`${Number("1.5kg")}`); // artinya dia ketat jika ada kg di belakang maka NaN
