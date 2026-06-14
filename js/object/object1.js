cc1b191150daf57531b18457ce8198668a1ae86e
Komang Aswameda <aswameda18@gmail.com>
2026-06-14 17:52:04 +0800

    selesai update object

--- js/object/object1.js
+++ js/object/object1.js
@@ -1,5 +1,10 @@
 // object ditandai dengan {} siku kurung
 
+/* 
+  brand: "honda" (property)
+  brand (key)
+  "honda" (value)
+*/
 var car = {};
 
 //notasi objek, lebih disukai
@@ -20,3 +25,29 @@ yourCar["year"] = 2012;
 // sama seperti associative-array, kita mengakses properti dengan key dalam kurung siku atau dot-notation
 console.log(yourCar.brand);
 console.log(yourCar['brand']);
+
+const person = {};
+
+person["nama"] = "hexs";
+person["umur"] = 20;
+person["negara"] = "indonesia";
+
+console.table(person);
+
+delete person["umur"];
+
+console.table(person);
+
+
+const orang = {
+  nama: "aswameda",
+  umur: 20,
+  negara: "indeonesia"
+}
+console.table(orang);
+
+// mengakses property object
+
+console.info(`nama : ${orang.nama}`);
+console.info(`umur : ${orang.umur}`);
+console.info(`negara : ${orang.negara}`);
