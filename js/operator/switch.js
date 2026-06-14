811f116df6cd2ee89ca8b2c8cad09e6970d26b6d
Komang Aswameda <aswameda18@gmail.com>
2026-06-14 19:53:37 +0800

    selesai belajar switch

--- js/operator/switch.js
+++ js/operator/switch.js
@@ -1,24 +1,29 @@
 // switch
 // Berdasarkan suatu kondisi, switch memilih satu atau lebih blok kode untuk dieksekusi
 
+// kadang kita hanya butuh menggunakan kondisi sederhana di if statement, seperti hanya menggunakan perbandingan ==
+// switch adalah statement percabangan yang sama dengan if, namun lebuh sederhana cara pembuatannya
+// kondisi di switch statement hanya untuk perbandingan ==
+
 // cara kerja
-switch (expression) {
-  case x:
-    // code block
-    break;
-  case y:
-    // code block
-    break;
-  default:
-  // code block
-}
+// switch (expression) {
+//   case x:
+//     // code block
+//     break;
+//   case y:
+//     // code block
+//     break;
+//   default:
+//   // code block
+// }
+
 // Beginilah cara kerjanya:
 // 1. Ekspresi switch dievaluasi sekali
 // 2. Nilai ekspresi tersebut dibandingkan dengan nilai dari setiap kasus
 // 3. Jika ditemukan kecocokan, blok kode yang terkait akan dieksekusi
 // 4. Jika tidak ada kecocokan, tidak ada kode yang dieksekusi.
 
-// contoh 
+// contoh
 let day;
 
 switch (new Date().getDay()) {
@@ -46,6 +51,7 @@ switch (new Date().getDay()) {
 }
 
 console.log(day);
+console.log("========================");
 // artinya dia mengambil hari di komputer kita yang di eksekusi adalah case yang betul jadi kalau salah dia lewati
 
 let x = 0;
@@ -61,3 +67,24 @@ switch (x) {
 }
 
 console.log(text);
+console.log("========================");
+
+const nilai = "B";
+
+switch (nilai) {
+  case "A":
+    console.log("dewa jirrr");
+    break;
+  case "B":
+  case "C":
+    console.log("hacker");
+    break;
+  case "D":
+    console.log("pro");
+    break;
+  case "E":
+    console.log("noob");
+    break;
+  default:
+    console.log("plenger jirrr, gk udah sekolah!!");
+}
