ef7b77de3eb2c7467da6105844f89df5f52760a5
Komang Aswameda <aswameda18@gmail.com>
2026-06-15 21:18:43 +0800

    selesai belajar function 8

--- js/function/function8.js
+++ js/function/function8.js
@@ -11,3 +11,26 @@ var myFunc = fn_A();//hello
 
 //myFunc sekarang berisi function yang //dikirim kembali oleh fn_A 
 myFunc();//world
+
+// function sebagai value
+// function tidak hanya bisa digunakan sebagai kode program yang dieksekusi, tapi bisa juga sebagai value
+// artinya, function bisa disimpan di variable, bisa juga dikirim melalui perameter function lainnya
+
+function sayHello(name) {
+    console.log(`hello ${name}`);
+}
+
+sayHello("udin");
+
+const say = sayHello;
+
+say("budi");
+
+
+// function di parameter
+function giveMeName(callback) {
+    callback("hexs");
+}
+
+giveMeName(sayHello);
+giveMeName(say);
