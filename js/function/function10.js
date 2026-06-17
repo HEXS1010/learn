b36d2949b3e89e1e9b1e96c785d385c8efd10736
Komang Aswameda <aswameda18@gmail.com>
2026-06-17 12:12:28 +0800

    selesai belajar function10

--- js/function/function10.js
+++ js/function/function10.js
@@ -8,3 +8,35 @@
 // tidak bisa menggunakan function generator
 // tidak bisa mengakses this
 // tidak bisa mengakses super
+
+const sayHello = (name) => {
+    const say = `hello ${name}`;
+    console.log(say);
+}
+
+sayHello("hexs");
+
+// arrow function tanpa blok
+// artinya tanpa {}
+// hanya satu baris
+
+const hello = (names) => console.log(`hello ${names}`);
+
+hello("hexs");
+
+// arrow function return value
+const sum =(first, second) => first + second;
+
+console.log(sum(100, 50));
+
+// arrow function tanpa kurung parameter
+const person = nama => console.log(`hello ${nama}`);
+
+// anonymous function
+giveName(function (name) {
+    console.log(`hello ${name}`);
+});
+
+
+// arrow sebagai sebagai parameter
+giveName((name) => console.log(`hello ${name}`));
