3ed0aecc311f81cc755cfa91d6e470d6763a1d7b
Komang Aswameda <aswameda18@gmail.com>
2026-06-19 13:45:24 +0800

    selsai belajar constructor function

---
+++ js2/constructor_function/constructor.js
@@ -0,0 +1,10 @@
+// OOP (object-oriented programming)
+// atau kita bisa sebut cara mengorganisir kode menggunakan object yang memiliki data dan fungsi 
+
+// EcmaScript6
+function Person() {
+
+}
+
+const aswa = new Person();
+const meda = new Person();
---
+++ js2/constructor_function/constructor1.js
@@ -0,0 +1,1 @@
+// property di constructor function
