e29cf44a367e268c14f9981a4a3d9c437817cf5f
Komang Aswameda <aswameda18@gmail.com>
2026-06-19 13:53:53 +0800

    selesai belajar contructor 2

--- js2/constructor_function/constructor1.js
+++ js2/constructor_function/constructor1.js
@@ -1,1 +1,16 @@
 // property di constructor function
+
+function Person() {
+    this.firstName = "";
+    this.lastName = "";
+}
+
+const aswa = new Person();
+aswa.firstName = "aswa";
+aswa.lastName = "meda";
+const udin = new Person();
+udin.firstName = "udin";
+udin.lastName = "nugraha";
+
+console.log(aswa);
+console.log(udin);
