8b7d97a85a98527ceb50510e540094a224e8acbb
Komang Aswameda <aswameda18@gmail.com>
2026-06-19 14:06:17 +0800

    selesai belajar cosntructor3

--- js2/constructor_function/constructor3.js
+++ js2/constructor_function/constructor3.js
@@ -1,22 +1,18 @@
 // parameter di constructor function
 
-function Person() {
-    this.firstName = "";
-    this.lastName = "";
+function Person(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
     this.sayHello = function (name) {
         console.log(`hello ${name}, my name is ${this.firstName}`);
     }
 }
 
-const aswa = new Person();
-aswa.firstName = "aswa";
-aswa.lastName = "meda";
+const aswa = new Person("aswa", "meda");
 aswa.sayHello("hexs");
 
 
-const udin = new Person();
-udin.firstName = "udin";
-udin.lastName = "nugraha";
+const udin = new Person("udin", "nugraha");
 udin.sayHello("hexs");
 
 console.log(aswa);
