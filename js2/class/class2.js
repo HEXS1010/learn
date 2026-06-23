7ef35b7a7996b574318599df7e550b66088e97a5
Komang Aswameda <aswameda18@gmail.com>
2026-06-23 22:09:27 +0800

    selesai belajar class 2

---
+++ js2/class/class2.js
@@ -0,0 +1,11 @@
+// property di class
+
+class person {
+    constructor(name) {
+        this.name = name;
+    }
+}
+
+const aswa = new person("aswa");
+console.log(aswa);
+console.log(aswa.name);
