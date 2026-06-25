fe97fc0edec4483c373682e2e41f2b490a645a03
Komang Aswameda <aswameda18@gmail.com>
2026-06-25 16:35:06 +0800

    selesai belajar class 8

---
+++ js2/class/class8.js
@@ -0,0 +1,20 @@
+// class field
+
+// public
+
+class Customer {
+    firstName;
+    lastName;
+    balance = 0;
+
+    constructor() {
+
+    }
+
+    sayHello() {
+
+    }
+}
+
+const aswa = new Customer();
+console.log(aswa);
