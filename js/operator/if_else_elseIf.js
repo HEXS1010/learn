4448a9ec509aa2664d33dec460534bade0c48445
HEXS1010 <aswameda18@gmail.com>
2026-09-23 19:16:57 +0800

    tambah operator logila

---
+++ js/operator/if_else_elseIf.js
@@ -0,0 +1,30 @@
+// kita akan belajar percabangan if else dan else if
+
+// kita akan bedah satu satu dari if, else dan else if
+
+// if
+let x = 10;
+
+if (x > 5) {
+    console.log("x lebih besar dari 5");
+}
+
+// else
+let y = 3;  
+
+if (y > 5) {
+    console.log("y lebih besar dari 5");
+} else {
+    console.log("y lebih kecil dari 5");
+}
+
+// else if
+let nilaiUjian = 80;
+
+if (nilaiUjian >= 90) {
+    console.log("nilai A");
+} else if (nilaiUjian >= 80) {
+    console.log("nilai B");
+} else {
+    console.log("nilai C");
+}
