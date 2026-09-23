715338b3f25a87bdbd6fa612ed367f2c1c851a2e
HEXS1010 <aswameda18@gmail.com>
2026-09-23 16:44:46 +0800

    ganti variabel 1

--- js/Variabel/variabel1.js
+++ js/Variabel/variabel1.js
@@ -1,7 +1,37 @@
-// deklarasi dan inisialisasi dalam satu baris
-var car_brand = 'honda';
+/*
+
+Variabel adalah sebuah nama yang mewakili sebuah nilai.
+Variabel bisa diisi dengan berbagai macam nilai seperti string
+(teks), number (angka), objek, array, dan sebagainya.
+
+*/
+
+// dalam javascript untuk yang sekarang lebih banya meggunakan let dan const abaikan var
+
+// let adalah variabel yang dimana dapat di ubah 
+let x = 5; 
+
+console.log(x);
+
+x = 10; // mengubah nilai x menjadi 10
+
+console.log(x);
+
+// sekarang kita masuk ke const
+const y = 2
+console.log(y); 
+
+y = 5; // ini akan error karena const tidak bisa di ubah nilainya
+console.log(y);
 
 
-// inisialisasi terpisah
-var car_brand;
-car_brand = 'honda';
+// dalam variabel dapat diisi string, boolean, number, object, array dan masih banyak lagi
+
+let sapa = "hallo dunia";
+let angka = 20;
+let kamuSukaAku = false;
+let person = {
+    name: "budi",
+    age: 30
+}
+let list = [1, 2, 3, 4, 5];
--- js/Variabel/variabel2.js
+++
@@ -1,8 +0,0 @@
-// semua dalam satu baris
-var brand = 'honda', type = 'MVP', numberofWheels = 4, price;
-
-// baris terpisah (lebih baik dari pada satu baris)
-var brand = 'honda',
-type = 'MVP',
-numberofWheels = 4,
-price;
