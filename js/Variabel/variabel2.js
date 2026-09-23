30fbccdea934f187fbbf427ee93f67f1ad0256f4
HEXS1010 <aswameda18@gmail.com>
2026-09-23 17:58:39 +0800

    ganti variabel 2

---
+++ js/Variabel/variabel2.js
@@ -0,0 +1,37 @@
+// variabel 2 kita akan belajar aritmatika dasar dalam javascript
+
+/* 
+    + tambah
+    - kurang 
+    * kali 
+    / bagi
+    ** pangkat
+    % sisa bagi
+*/
+
+const a = 10;
+const b = 5;
+
+console.log(a + b); // 15
+console.log(a - b); // 5
+console.log(a * b); // 50
+console.log(a / b); // 2
+console.log(a ** b); // 100000
+console.log(a % b); // 0        
+
+// ada juga Operator Assignment  
+
+/*
+    += (x = x + 1)
+    -= (x = x - 1)
+    *= (x = x * 1)
+    /= (x = x / 1)
+    
+*/
+
+let c = 20;
+
+console.log(c += 1);
+console.log(c -= 1);
+console.log(c *= 1);
+console.log(c /= 1);
